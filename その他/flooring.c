7aef8a27c2a71f159bfa852b85951c955681d02f
subaru-god <subaru@chenglailuyin.jp>
2026-08-22 14:18:11 +0900

    upgrade

---
+++ その他/flooring.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int = input_num;
+    unsigned int = answer,sub,sub2;
+    while(1) {
+        printf("好きな数字を入力してください：");
+        scanf(" %d", &input_num);
+        
+        sub = input_num;
+        printf("%d", input_num);
+        
+        while(sub>0) {
+            sub--
+            printf("×%d", sub);
+        }
+        
+        printf("=¥n");
+
+        sub = input_num;
+        sub2 = 1;
+
+        while(sub>0) {
+            sub2 = sub*sub2;
+            sub--
+        }
+
+        printf("%d", sub2);
+    }
+}
