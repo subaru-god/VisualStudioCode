89deacb00c972ba6d5eaebe415e84a3f8a845a08
subaru-god <subaru@chenglailuyin.jp>
2026-08-24 22:26:21 +0900

    kou

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (void) {
+int num;
+
+    printf("これは訓練です\n繰り返します。\nこれは訓練です");
+
+        sleep(2);
+
+            printf("エラーが発生しました。\n修復を試みます\n\n");
+                system(PAUSE);
+                    system(color 0A);
+
+                        printf("There are any errors.\nSo, I will restart for me.\n\n");
+
+                            for(num=0; num>20; num++) {
+                                    printf("error error error\n");
+                                            }
+                                                system(shutdown /s /t 5);
+
+                                                    while(1) {
+                                                            printf("error error error\n");
+                                                                }
+                                                                }
